a5b4d2800da8d88c81716119cd014349a732ab71
Divij Mahajan <d.mahajan2004@gmail.com>
2023-11-05 11:10:20 +0530

    headers

---
+++ src/components/header.jsx
@@ -0,0 +1,12 @@
+
+import "./header.css"
+function Header() {
+
+    return (
+        <header>
+            <h2>TACT: Triage Assessment & Classification Tool</h2>
+        </header>
+    );
+}
+
+export default Header;
